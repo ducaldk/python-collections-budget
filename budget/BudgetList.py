a8125f54d1fb84421ba9a67d203d7beaded3814a
Dave Kessler <Dave.Kessler@sas.com>
2023-05-01 13:08:08 -0400

    Completed module 3

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,5 @@
 from . import Expense
+import matplotlib.pyplot as plt
 
 class BudgetList:
     
@@ -23,6 +24,25 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses)+len(self.overages)
     
+    # module 3 tasks 1,2
+    def __iter__(self):
+        # not explained in the instructions but figure-outable?
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+    
+    # module 3 tasks 4,5
+    def __next__(self):
+        try:
+            # this was not acceptable to the test but passed the boundary conditions
+            # return next(self.iter_e)
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            # this was not acceptable to the test but passed the boundary conditions
+            # return next(self.iter_o)
+            return self.iter_o.__next__()
+
+    
 
 # task 7
 def main():
@@ -37,7 +57,25 @@ def main():
 
     # task 11
     print( "The count of all expenses: " + str(len(myBudgetList)))
-    print(f"The count of all expenses: {len(myBudgetList)}")
+    # print(f"The count of all expenses: {len(myBudgetList)}")
+
+    # module 3, task 5
+    for entry in myBudgetList:
+        print(entry)
+
+    # module 3, task 7
+    fig, ax = plt.subplots()
+
+    # module 3, task 8
+    labels = [ 'Expenses', 'Overages', 'Budget']
+    # module 3, task 9
+    values = [ myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    # module 3, task 10
+    ax.bar(labels, values, color=['green','red','blue'])
+    # module 3, task 11
+    ax.set_title('Your total expenses vs. total budget')
+    # module 3, task 12
+    plt.show()
 
 
 # task 12
